635777c95a2896409ac2f60b74445242c4d61c45
AcoMat <aaco.maty@gmail.com>
2025-04-08 23:59:31 -0300

    renamed wrong folder name and added new microservice

---
+++ meli-clone-ms-products/src/main/java/com/meli_clone/ms_products/MicroservicioProductosApiMeLiApplication.java
@@ -0,0 +1,13 @@
+package com.meli_clone.ms_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioProductosApiMeLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioProductosApiMeLiApplication.class, args);
+	}
+
+}
---
+++ meli-clone-ms-products/src/test/java/com/meli_clone/ms_products/MicroservicioProductosApiMeLiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli_clone.ms_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioProductosApiMeLiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
